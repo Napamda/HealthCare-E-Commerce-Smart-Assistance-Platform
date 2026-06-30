d38283e70977907ab749682d84a228d504e06adc
Nana Dankwa <kwadwodankwa345@gmail.com>
2026-06-30 11:36:28 +0800

    Initial Commit

---
+++ Backend/src/main/java/org/example/healthcareecommercesmartassistanceplatform/HealthCareECommerceSmartAssistancePlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.healthcareecommercesmartassistanceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareECommerceSmartAssistancePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCareECommerceSmartAssistancePlatformApplication.class, args);
+    }
+
+}
